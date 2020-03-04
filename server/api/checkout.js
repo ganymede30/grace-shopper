86336508512724ce3510f683968d6ade1ab55668
richmcateer <richfullstack@gmail.com>
2020-03-04 14:10:14 -0500

    Configure Stripe on backend

--- server/api/checkout.js
+++ server/api/checkout.js
@@ -1,26 +1,65 @@
 const router = require('express').Router()
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const {Shoe} = require('../db/models')
+const {Op} = require('sequelize')
 
-const calculateOrderAmount = items => {
-  // Replace this constant with a calculation of the order's amount
-  // Calculate the order total on the server to prevent
-  // people from directly manipulating the amount on the client
-  return 1400
+const calculateOrderAmount = products => {
+  let total = 0
+  products.forEach(product => {
+    if (!product.price) {
+      throw new Error('Product is missing a price')
+    } else if (!product.quantity) {
+      throw new Error('Product is missing a quantity')
+    } else {
+      total += product.price * product.quantity
+    }
+  })
+  return total
 }
 
-router.post('/create-payment-intent', async (req, res) => {
-  const {items, currency} = req.body
-  // Create a PaymentIntent with the order amount and currency
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(items),
-    currency: currency
+router.post('/create-payment-intent', async (req, res, next) => {
+  // Each item must have an id and quantity
+  const {items} = req.body
+
+  // Get the product id from items in the request
+  const productIds = items.map(item => {
+    return {id: item.id}
   })
 
-  // Send publishable key and PaymentIntent details to client
-  res.send({
-    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-    clientSecret: paymentIntent.client_secret
+  // Fetch corresponding products from the database
+  const products = await Shoe.findAll({
+    attributes: ['id', 'price'],
+    where: {
+      [Op.or]: productIds
+    }
   })
+
+  // Store the prices in a hash map
+  const priceMap = {}
+  products.forEach(product => {
+    priceMap[product.id] = product.price
+  })
+
+  // Add the price to the item => (id, quantity, price)
+  const pricedItems = items.map(item => {
+    return {...item, price: priceMap[item.id]}
+  })
+
+  try {
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: calculateOrderAmount(pricedItems),
+      currency: 'USD'
+    })
+
+    // Send publishable key and PaymentIntent details to client
+    res.send({
+      publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+      clientSecret: paymentIntent.client_secret
+    })
+  } catch (e) {
+    next(e)
+  }
 })
 
 module.exports = router
--- server/api/checkout.spec.js
+++ server/api/checkout.spec.js
@@ -4,10 +4,23 @@ const {expect} = require('chai')
 const request = require('supertest')
 const db = require('../db')
 const app = require('../index')
+const Shoe = db.model('shoe')
 
 describe('Checkout routes', () => {
-  beforeEach(() => {
-    return db.sync({force: true})
+  beforeEach(async () => {
+    await db.sync({force: true})
+
+    const shoe1 = {
+      brand: 'Nike',
+      price: 500 // $5
+    }
+    const shoe2 = {
+      brand: 'Nike',
+      price: 1000 // $10
+    }
+
+    await Shoe.create(shoe1)
+    await Shoe.create(shoe2)
   })
 
   describe('/api/checkout', () => {
@@ -15,12 +28,23 @@ describe('Checkout routes', () => {
       const res = await request(app)
         .post('/api/checkout/create-payment-intent')
         .send({
-          currency: 'USD',
-          items: []
+          items: [
+            {id: 1, quantity: 1},
+            {id: 2, quantity: 3}
+          ]
         })
         .expect(200)
       expect(res.body.publishableKey).to.be.a('string')
       expect(res.body.clientSecret).to.be.a('string')
+    })
+
+    it('Requires a quantity', async () => {
+      await request(app)
+        .post('/api/checkout/create-payment-intent')
+        .send({
+          items: [{id: 1}, {id: 2}]
+        })
+        .expect(500)
     })
   })
 })

7a25857e4c860e83039cb6e71e85d2d0fa57cf44
Carlos de la Cruz <carlos.cruz1500@gmail.com>
2020-03-05 19:11:18 -0500

    finished basic styling for cart

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,120 +1,129 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, Fragment} from 'react'
 import {connect} from 'react-redux'
-// import CartItem from './CartItem'
 import {getAllItemsThunk} from '../store/cart'
 import {makeStyles} from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
 import TableContainer from '@material-ui/core/TableContainer'
 import TableHead from '@material-ui/core/TableHead'
-import TablePagination from '@material-ui/core/TablePagination'
 import TableRow from '@material-ui/core/TableRow'
+import {Paper, Grid} from '@material-ui/core/'
 
-const columns = [
-  {id: 'product', label: 'Product', minWidth: 100},
-  {id: 'info', label: 'Product Info', minWidth: 100},
-  {
-    id: 'quantity',
-    label: 'Qty',
-    minWidth: 40,
-    align: 'right',
-    format: value => value.toLocaleString()
-  },
-  {
-    id: 'price',
-    label: 'Price',
-    minWidth: 40,
-    align: 'right',
-    format: value => value.toLocaleString()
-  }
-]
-
-function createData(product, info, quantity, price) {
-  return {product, info, quantity, price}
-}
+const TAX_RATE = 0.0
 
 const useStyles = makeStyles({
-  root: {
-    width: '60%'
-  },
-  container: {
-    maxHeight: 440
+  table: {
+    width: '100%'
   }
 })
+
+function ccyFormat(num) {
+  return `${num.toFixed(2)}`
+}
+
+function priceRow(qty, unit) {
+  return qty * unit
+}
+
+function createRow(product, productInfo, qty = 0, unitPrice = 0) {
+  const price = priceRow(qty, unitPrice)
+  return {product, productInfo, qty, unitPrice, price}
+}
+
+function subtotal(items) {
+  return items.map(({price}) => price).reduce((sum, i) => sum + i, 0)
+}
 
 const Cart = ({items, fetchCart}) => {
   useEffect(() => {
     fetchCart()
   }, [])
 
-  console.log(items, 'Items')
   const rows = items.map(item =>
-    createData(item.imageUrl, item.model, item.qty, item.price, item.total)
+    createRow(item.imageUrl, item.model, item.qty, item.price)
   )
+  console.log(rows, 'rows?')
 
+  const invoiceSubtotal = subtotal(rows)
+  const invoiceTaxes = TAX_RATE * invoiceSubtotal
+  const invoiceTotal = invoiceTaxes + invoiceSubtotal
   const classes = useStyles()
-  const [page, setPage] = React.useState(0)
-  const [rowsPerPage, setRowsPerPage] = React.useState(10)
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage)
-  }
-  const handleChangeRowsPerPage = event => {
-    setRowsPerPage(+event.target.value)
-    setPage(0)
-  }
 
   return (
-    <Paper className={classes.root}>
-      <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map(column => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{minWidth: column.minWidth}}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map(row => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                    {columns.map(column => {
-                      const value = row[column.id]
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      )
-                    })}
+    <Fragment>
+      <Grid container>
+        <Grid item xs={8} style={{padding: '1% 2%'}}>
+          <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Product</TableCell>
+                  <TableCell>Product Info</TableCell>
+                  <TableCell align="right">Qty</TableCell>
+                  <TableCell align="right">Unit Price</TableCell>
+                  <TableCell align="right">Total</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map(row => (
+                  <TableRow key={row.product}>
+                    <TableCell>
+                      <div>
+                        <img
+                          style={{
+                            height: 130,
+                            backgroundSize: 'auto'
+                          }}
+                          src={row.product}
+                        />
+                      </div>
+                    </TableCell>
+                    <TableCell>{row.productInfo}</TableCell>
+                    <TableCell>{row.qty}</TableCell>
+                    <TableCell>{ccyFormat(row.unitPrice)}</TableCell>
+                    <TableCell>{ccyFormat(row.price)}</TableCell>
                   </TableRow>
-                )
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onChangePage={handleChangePage}
-        onChangeRowsPerPage={handleChangeRowsPerPage}
-      />
-    </Paper>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        {/* <Paper style={{ height: 'fit-content' }}> */}
+
+        <Grid item xs={4} style={{padding: '1% 2%'}}>
+          <TableContainer style={{width: '70%'}} component={Paper}>
+            <Table aria-label="table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Summary</TableCell>
+                  <TableCell align="right">{items.length} Items</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell style={{borderBottom: 'unset'}}>
+                    Subtotal
+                  </TableCell>
+                  <TableCell style={{borderBottom: 'unset'}} align="right">
+                    {ccyFormat(invoiceSubtotal)}
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Tax</TableCell>
+                  <TableCell align="right">{ccyFormat(invoiceTaxes)}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Total</TableCell>
+                  <TableCell align="right">{ccyFormat(invoiceTotal)}</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        {/* </Paper> */}
+      </Grid>
+    </Fragment>
   )
 }
 

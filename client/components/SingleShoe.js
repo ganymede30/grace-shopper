54fb1b5ab6a7ebaccbb2a0bc82db6c431b06f531
Michael Kenny <mfkenny130@gmail.com>
2020-03-10 17:44:28 -0400

    Add to cart for single shoe

--- client/components/SingleShoe.js
+++ client/components/SingleShoe.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {getShoeThunk} from '../store/shoes'
+import {addToOrderThunk, addToOrderGuestThunk} from '../store/order'
 import {Shoe} from './Shoe'
+import {Button} from '@material-ui/core/'
 
 export class SingleShoe extends Component {
   constructor(props) {
@@ -13,16 +15,37 @@ export class SingleShoe extends Component {
     await this.props.fetchShoe(id)
   }
   render() {
-    return <Shoe shoe={this.props.shoe} />
+    const {user, shoe} = this.props
+
+    return (
+      <div>
+        <Shoe shoe={this.props.shoe} />
+        <Button
+          style={{marginLeft: '0 auto', marginRight: '0 auto'}}
+          variant="contained"
+          type="submit"
+          onClick={
+            user.id
+              ? () => this.props.addToCart(shoe)
+              : () => this.props.addToCartGuest(shoe)
+          }
+        >
+          Add to cart
+        </Button>
+      </div>
+    )
   }
 }
 
 const mapState = state => ({
-  shoe: state.shoes.shoe
+  shoe: state.shoes.shoe,
+  user: state.user
 })
 
 const mapDispatch = dispatch => ({
-  fetchShoe: id => dispatch(getShoeThunk(id))
+  fetchShoe: id => dispatch(getShoeThunk(id)),
+  addToCart: item => dispatch(addToOrderThunk(item)),
+  addToCartGuest: item => dispatch(addToOrderGuestThunk(item))
 })
 
 export default connect(mapState, mapDispatch)(SingleShoe)
